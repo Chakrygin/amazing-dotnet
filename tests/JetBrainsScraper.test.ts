f919d807901775d25c43320a98499be1460a7e6e
Igor Chakrygin <igor@chakrygin.ru>
2022-05-17 22:49:10 +0300

    Fix jetbrains scraper tests

--- tests/JetBrainsScraper.test.ts
+++ tests/JetBrainsScraper.test.ts
@@ -3,16 +3,16 @@ import JetBrainsScraper from '../src/scrapers/JetBrainsScraper';
 import { testScraper } from './helpers';
 
 test('JetBrains / How-To\'s', async () => {
-  const scraper = new JetBrainsScraper('how-tos');
+  const scraper = new JetBrainsScraper('category/how-tos');
   await testScraper(scraper);
 });
 
 test('JetBrains / Releases', async () => {
-  const scraper = new JetBrainsScraper('releases');
+  const scraper = new JetBrainsScraper('category/how-tos');
   await testScraper(scraper);
 });
 
 test('JetBrains / .NET Annotated', async () => {
-  const scraper = new JetBrainsScraper('net-annotated');
+  const scraper = new JetBrainsScraper('tag/net-annotated');
   await testScraper(scraper);
 });
