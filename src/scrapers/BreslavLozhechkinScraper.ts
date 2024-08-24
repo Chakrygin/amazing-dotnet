e331d4e46b22317d158a9f656a655c2da6b66e11
Igor Chakrygin <igor@chakrygin.ru>
2024-08-24 19:32:02 +0300

    Refactor RadioDotNetScraper and BreslavLozhechkinScraper

--- src/scrapers/BreslavLozhechkinScraper.ts
+++ src/scrapers/BreslavLozhechkinScraper.ts
@@ -1,110 +1,57 @@
-import * as cheerio from 'cheerio';
-import moment from 'moment';
+import { MaveEpisodeData, MaveScraperBase } from '@core/scrapers/shared';
 
-import { Link, Post } from '@core/models';
-import { ScraperBase } from '@core/scrapers';
-
-export class BreslavLozhechkinScraper extends ScraperBase {
-  readonly name = 'BreslavLozhechkin';
-  readonly path = 'breslav-lozhechkin.mave.digital';
-
-  private readonly BreslavLozhechkin: Link = {
-    title: 'Бреслав и Ложечкин',
-    href: 'https://breslav-lozhechkin.mave.digital',
-  };
-
-  protected fetchPosts(): AsyncGenerator<Post> {
-    return this
-      .fromHtmlPage(this.BreslavLozhechkin.href)
-      .fetchPosts('.episodes-season__body a.episode', ($, element) => {
-
-        const image = $('.sidebar__header .img-wrapper img').attr('src') ?? '';
-        const release = element.find('.episode-body__content .release').text().trim();
-        const title = element.find('.episode-body__content .description').text().trim();
-        const href = element.attr('href') ?? '';
-        const date = element.find('.episode-body__content .date').text();
-
-        return {
-          image,
-          title: `${release}. ${title}`,
-          href: this.getFullHref(href),
-          categories: [this.BreslavLozhechkin],
-          date: moment(date, 'LL', 'ru'),
-        };
-
-      });
+export class BreslavLozhechkinScraper extends MaveScraperBase {
+  constructor() {
+    super('breslav-lozhechkin', {
+      name: 'BreslavLozhechkin',
+      title: 'Бреслав и Ложечкин',
+      storage: 'https://ru-msk-dr3-1.store.cloud.mts.ru/mave'
+    });
   }
 
-  protected override enrichPost(post: Post): Promise<Post | undefined> {
-    return this
-      .fromHtmlPage(post.href)
-      .enrichPost('main>div.single-page-wrapper', ($, element) => {
+  protected override getDescription(episode: MaveEpisodeData): string[] | undefined {
+    const description: string[] = [];
 
-        const description = this.getDescription($, element);
-        const links = this.getLinks($, element);
+    const lines = this.getDescriptionLines(episode);
 
-        post = {
-          ...post,
-          description: [description],
-          links: [
-            ...links,
-            {
-              title: 'Слушать на Mave',
-              href: post.href,
-            }
-          ],
-        };
+    let flag = true;
+    let links: string[] | null = null;
 
-        return post;
-      });
-  }
+    for (const line of lines) {
+      if (line.includes('Реклама')) {
+        break;
+      }
 
-  private getDescription($: cheerio.CheerioAPI, element: cheerio.Cheerio<cheerio.Element>): string {
-    const node = element
-      .find('div.episodes-info__body>div.description.html-content')
-      .contents()
-      .first();
-
-    const description = $(node).text();
-    return description;
-  }
-
-  private getLinks($: cheerio.CheerioAPI, element: cheerio.Cheerio<cheerio.Element>): Link[] {
-    const result: Link[] = [];
-    const links = element
-      .find('.listen a.listen-platform-wrapper')
-      .filter('.yandex-music, .youtube')
-      .map((_, link) => $(link));
-
-    for (const link of links) {
-      const href = link.hasClass('yandex-music')
-        ? link.attr('href')?.replace('music.yandex.com', 'music.yandex.ru')
-        : link.attr('href');
-
-      if (href) {
-        if (link.hasClass('yandex-music')) {
-          result.push({
-            title: 'Слушать на Яндекс.Музыке',
-            href
-          });
+      if (line) {
+        if (links) {
+          links.push(line);
         }
-        else if (link.hasClass('youtube')) {
-          result.push({
-            title: 'Слушать на YouTube',
-            href
-          });
+        else if (line.endsWith(':')) {
+          links = [line];
         }
+        else if (flag) {
+          description.push(line);
+        }
+        else {
+          break;
+        }
+      }
+      else if (links) {
+        description.push(
+          links.join('\n'));
+
+        flag = false;
+        links = null;
       }
     }
 
-    return result;
-  }
-
-  private getFullHref(href: string): string {
-    if (href.startsWith('/')) {
-      href = this.BreslavLozhechkin.href + href;
+    if (links) {
+      description.push(
+        links.join('\n'));
     }
 
-    return href;
+    if (description.length > 0) {
+      return description;
+    }
   }
 }
--- src/scrapers/RadioDotNetScraper.ts
+++ src/scrapers/RadioDotNetScraper.ts
@@ -1,74 +1,20 @@
-import * as cheerio from 'cheerio';
-import moment from 'moment';
+import { Link } from '@core/models';
+import { MaveEpisodeData, MavePodcastData, MaveScraperBase } from '@core/scrapers/shared';
 
-import { Link, Post } from '@core/models';
-import { ScraperBase } from '@core/scrapers';
-
-export class RadioDotNetScraper extends ScraperBase {
-  readonly name = 'RadioDotNet';
-  readonly path = 'radiodotnet.mave.digital';
-
-  private readonly RadioDotNet: Link = {
-    title: 'RadioDotNet',
-    href: 'https://radiodotnet.mave.digital',
-  };
-
-  protected override fetchPosts(): AsyncGenerator<Post> {
-    return this
-      .fromHtmlPage(this.RadioDotNet.href)
-      .fetchPosts('.episodes-season__body a.episode', ($, element) => {
-
-        const image = $('.sidebar__header .img-wrapper img').attr('src') ?? '';
-        const release = element.find('.episode-body__content .release').text().trim();
-        const title = element.find('.episode-body__content .description').text().trim();
-        const href = element.attr('href') ?? '';
-        const date = element.find('.episode-body__content .date').text();
-
-        return {
-          image,
-          title: `${release}. ${title}`,
-          href: this.getFullHref(href),
-          categories: [this.RadioDotNet],
-          date: moment(date, 'LL', 'ru'),
-        };
-
-      });
+export class RadioDotNetScraper extends MaveScraperBase {
+  constructor() {
+    super('radiodotnet', {
+      name: 'RadioDotNet',
+      title: 'RadioDotNet',
+      storage: 'https://ru-msk-dr3-1.store.cloud.mts.ru/mave'
+    });
   }
 
-  protected override enrichPost(post: Post): Promise<Post | undefined> {
-    return this
-      .fromHtmlPage(post.href)
-      .enrichPost('main>div.single-page-wrapper', ($, element) => {
-
-        const description = this.getDescription($, element);
-        const links = this.getLinks($, element);
-
-        post = {
-          ...post,
-          description: [description],
-          links: [
-            ...links,
-            {
-              title: 'Слушать на Mave',
-              href: post.href,
-            },
-            {
-              title: 'Поддержать на Boosty',
-              href: 'https://boosty.to/radiodotnet',
-            },
-          ],
-        };
-
-        return post;
-      });
-  }
-
-  private getDescription($: cheerio.CheerioAPI, element: cheerio.Cheerio<cheerio.Element>): string {
+  protected override getDescription(episode: MaveEpisodeData): string | undefined {
     const description = [];
+
     const regexp = /^\[\d{2}:\d{2}:\d{2}\]/;
-    const lines = element
-      .find('div.episodes-info__body>div.description>p')
-      .map((_, line) => $(line).text().trim());
+    const lines = this.getDescriptionLines(episode);
 
     for (const line of lines) {
       if (regexp.test(line)) {
@@ -79,46 +25,16 @@ export class RadioDotNetScraper extends ScraperBase {
     if (description.length > 0) {
       return description.join('\n');
     }
-
-    return $('.sidebar__body p.description').text().trim();
   }
 
-  private getLinks($: cheerio.CheerioAPI, element: cheerio.Cheerio<cheerio.Element>): Link[] {
-    const result: Link[] = [];
-    const links = element
-      .find('.listen a.listen-platform-wrapper')
-      .filter('.yandex-music, .youtube')
-      .map((_, link) => $(link));
+  protected override getLinks(podcast: MavePodcastData, episode: MaveEpisodeData): Link[] {
+    const links = this.getDefaultLinks(podcast, episode);
 
-    for (const link of links) {
-      const href = link.hasClass('yandex-music')
-        ? link.attr('href')?.replace('music.yandex.com', 'music.yandex.ru')
-        : link.attr('href');
+    links.push({
+      title: 'Поддержать на Boosty',
+      href: 'https://boosty.to/radiodotnet',
+    });
 
-      if (href) {
-        if (link.hasClass('yandex-music')) {
-          result.push({
-            title: 'Слушать на Яндекс.Музыке',
-            href
-          });
-        }
-        else if (link.hasClass('youtube')) {
-          result.push({
-            title: 'Слушать на YouTube',
-            href
-          });
-        }
-      }
-    }
-
-    return result;
-  }
-
-  private getFullHref(href: string): string {
-    if (href.startsWith('/')) {
-      href = this.RadioDotNet.href + href;
-    }
-
-    return href;
+    return links;
   }
 }
